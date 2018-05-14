f7537e4665447f53b3fe13a6c5f26a761ada5aa8
Jackobbo <szczszcz@gmail.com>
2018-05-15 01:32:55 +0200

    Commit sprawdzajacy poprawnosc dzialania

--- app/src/main/java/com/example/jakubtomaszewski/lab4/MainActivity.java
+++ app/src/main/java/com/example/jakubtomaszewski/lab4/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.jakubtomaszewski.lab4;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//sprawdzam poprawnosc dzialania commita
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
